7f8256afb220f1c0b0b4237a33fbff099c787caf
Kazuhiro Misumi <misumi@aspen.abiko.denken.or.jp>
2015-12-17 15:41:51 +0900

    Refine the example.

--- nc_out.m
+++ nc_out.m
@@ -1,10 +1,14 @@
-my_data = linspace(0,49,50);
+% NOCLOBBER prevents overwriting the existing file.
+% CLOBBER overwrites the existing file.
+ncid = netcdf.create('nc_out.nc','CLOBBER');
+lon_dimid = netcdf.defDim(ncid,'nlon',360);
+lat_dimid = netcdf.defDim(ncid,'nlat',180);
+time_dimid = netcdf.defDim(ncid,'time',netcdf.getConstant('NC_UNLIMITED'));
 
-% NOCLOBBER prevents overwriting when the same filename exists.
-ncid = netcdf.create('my_file.nc','NOCLOBBER');
-dimid = netcdf.defDim(ncid,'my_dim',50);
-varid = netcdf.defVar(ncid,'my_var','NC_BYTE',dimid)
+% note that matlab adopt the fortran-style order.
+varid = netcdf.defVar(ncid,'temp','double',[lon_dimid,lat_dimid,time_dimid]);
+
 netcdf.endDef(ncid) % This command terminates the definition mode; to write data into files, the mode should be changed to the data mode.
 
-netcdf.putVar(ncid,varid,my_data)
+netcdf.putVar(ncid,varid,[0,0,0],[360,180,1],zeros(360,180,1));
 netcdf.close(ncid)
